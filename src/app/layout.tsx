07501230f64b488b7c988ababd1094c0903c5a40
Filipe Moreno <y8.filipe@gmail.com>
2024-05-17 11:33:02 -0300

    animação do texto "Em breve"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,38 +1,37 @@
 import type { Metadata } from "next";
-import { Inter, Finger_Paint } from "next/font/google";
+import { Finger_Paint, Inter } from "next/font/google";
 import "@/styles/globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
-const fingerpaint = Finger_Paint({ subsets: ["latin"], weight: "400" })
-
+const fingerpaint = Finger_Paint({ subsets: ["latin"], weight: "400" });
 
 export const metadata: Metadata = {
-  title: "LTNES",
-  description: "Site do LTNES",
-  openGraph: {
-    title: "The LTNES",
-    description: "Um APP incrível para pessoas incríveis",
-    type: "website",
-    url: "https://ltnes.com.br",
-    images: ['/opengraph-image.jpg'],
-    locale: 'pt-BR'
-  },
-  twitter: {
-    card: "summary_large_image",
-    images: ['/opengraph-image.jpg'],
-    description: "Um APP incrível para pessoas incríveis",
-    title: "The LTNES"
-  },
+	title: "LTNES",
+	description: "Site do LTNES",
+	openGraph: {
+		title: "The LTNES",
+		description: "Um APP incrível para pessoas incríveis",
+		type: "website",
+		url: "https://ltnes.com.br",
+		images: "/opengraph-image.jpg",
+		locale: "pt-BR",
+	},
+	twitter: {
+		card: "summary_large_image",
+		images: "/opengraph-image.jpg",
+		description: "Um APP incrível para pessoas incríveis",
+		title: "The LTNES",
+	},
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="pt-BR">
-      <body className={fingerpaint.className}>{children}</body>
-    </html>
-  );
+	return (
+		<html lang="pt-BR">
+			<body className={fingerpaint.className}>{children}</body>
+		</html>
+	);
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,36 @@
+"use client";
 import Image from "next/image";
+import { useEffect, useRef } from "react";
+import Typed from "typed.js";
 
 export default function Home() {
-  return (
-    <main className="flex h-full w-full flex-col items-center justify-center">
-    <Image
-      className="relative"
-      src="/logo.svg"
-      alt="Ltnes Logo"
-      width={600}
-      height={400}
-      priority
-    />
-    <div>
-      <h1 className="text-4xl -mt-16">EM BREVE</h1>
-    </div>
-    </main>
-  );
+	const el = useRef(null);
+	useEffect(() => {
+		const typed = new Typed(el.current, {
+			strings: ["Em Breve", "Coming Soon", "À Venir", "Prossimamente"],
+			typeSpeed: 50,
+			loop: true,
+			backSpeed: 50,
+			cursorChar: "",
+		});
+		return () => {
+			typed.destroy();
+		};
+	}, []);
+
+	return (
+		<main className="flex h-full w-full flex-col items-center justify-center">
+			<Image
+				className="relative"
+				src="/logo.png"
+				alt="Ltnes Logo"
+				width={600}
+				height={400}
+				priority
+			/>
+
+			{/* biome-ignore lint/a11y/useHeadingContent: <explanation> */}
+			<h1 className="text-4xl" ref={el} />
+		</main>
+	);
 }
